89a4aa0765f06aa1b1911ff91dcf983f22b9bb0b
pham van vu <phamvanvuhb1999@gmail.com>
2020-08-17 21:08:43 +0700

    add new feature

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
 const express = require('express');
-const app = express()
+var bodyParser = require('body-parser');
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('db.json');
+var db = low(adapter);
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] })
+  .write();
+
+const app = express();
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+// parse application/json
+app.use(bodyParser.json());
+var id = 2;
+
 
 app.get('/', function(req,res){
-	res.send('<h1>hello there !</h1>');
+	res.render('index.pug', {
+		name: 'aaa'
+	});
+});
+
+app.get('/users', function(req,res){
+	res.render('users/index.pug',{
+		users: db.get('users').value()
+	});
+});
+
+app.get('/users/search', function(req, res){
+	console.log(req.query);
+	var q = req.query.q;
+	var matchedUsers = db.get('users').value().filter(function(user){
+		return user.name.indexOf(q) !== -1;
+	});
+	res.render('users/index', {
+		users: matchedUsers
+	});
+});
+app.get('/users/create', function(req,res){
+	res.render('users/create');
+});
+app.post('/users/create', function(req,res){
+	var temp = {
+		id: ++id, name: req.body.name
+	}
+	db.get('users').push(temp).write()
+	res.redirect('/users');
 });
 
 app.listen(3000, function(){
 	console.log('app is listening on port 3000 !');
-})
+});
